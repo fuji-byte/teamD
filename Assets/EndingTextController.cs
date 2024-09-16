df38ab7dac0d4a341a0e103ede930a5aa6703ebc
YusukeMatsumotoGG <k272071@kansai-u.ac.jp>
2024-09-16 18:09:01 +0900

    Zenゴシックフォント追加／エンディングシーン追加／効果音追加

---
+++ Assets/EndingTextController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class EndingTextController : MonoBehaviour
+{
+    public TextMeshProUGUI textLabel;
+    public TextMeshProUGUI bottomText;
+    public string[] word;
+    public AudioClip sound;
+    private int num = 0;
+ 
+    private void Start()
+    {
+        textLabel.text = word[num];
+    }
+ 
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(num == word.Length - 1)
+            {
+                SceneManager.LoadScene();
+            }
+ 
+            AudioSource.PlayClipAtPoint(sound, Camera.main.transform.position, 0.5f);
+ 
+            num += 1;
+ 
+            textLabel.text = word[num];
+            if(num == word.Length - 1){
+                bottomText.gameObject.SetActive(true);
+            }
+        }
+    }
+}
