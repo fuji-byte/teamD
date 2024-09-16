e7afe7c6147a9a10b4de06d340de20f0771e7fc7
YusukeMatsumotoGG <k272071@kansai-u.ac.jp>
2024-09-17 02:19:00 +0900

    タスク間の待機画面実装／タスクの効果音追加

--- Assets/mkaimura/yu-syacontraler.cs
+++ Assets/mkaimura/yu-syacontraler.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class HeroAttack : MonoBehaviour
 {
     private Animator animator;
+    public AudioClip swordSound;
+    public GameController gameControllerScript;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        gameControllerScript = GameObject.Find("GameController").GetComponent<GameController>();
     }
 
     void Update()
@@ -14,7 +17,10 @@ public class HeroAttack : MonoBehaviour
         if(TaskLogic.rdm == 1){
             if (Input.GetMouseButtonDown(0)) // ���N���b�N
             {
+                if(gameControllerScript.MKI_gameFinished == false){
                 animator.SetTrigger("Attack");
+                AudioSource.PlayClipAtPoint(swordSound, Camera.main.transform.position, 0.25f);
+                }
             }
         }
     }
