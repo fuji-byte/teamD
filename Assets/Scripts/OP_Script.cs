58eafafe1500f8b7a8aade128d3ff38bd3682455
Yuto Hiramatsu <hira.yuto1107@icloud.com>
2024-09-17 02:29:39 +0900

    OPシーン

---
+++ Assets/Scripts/OP_Script.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class TimelineSceneChanger : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public string RunGameScene_smartphone_system;
+    private int flag = 0;
+
+    void Start()
+    {
+        if (playableDirector != null)
+        {
+            playableDirector.stopped += OnPlayableDirectorStopped;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            flag = 1;
+            SkipScene();
+        }
+    }
+
+    void OnPlayableDirectorStopped(PlayableDirector director)
+    {
+        if (flag == 0)
+        {
+            if (director == playableDirector)
+            {
+                SkipScene();
+            }
+        }
+    }
+
+    void SkipScene()
+    {
+        SceneManager.LoadScene(RunGameScene_smartphone_system);
+    }
+
+    void OnDestroy()
+    {
+        if (playableDirector != null)
+        {
+            playableDirector.stopped -= OnPlayableDirectorStopped;
+        }
+    }
+}
